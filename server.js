98be6aa567dfb7315b1a2c43d73ff2c6a5d86ad1
Seth Weiner <sweiner@fonteva.com>
2020-07-10 17:10:17 -0400

    init

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ const express = require('express')
 const app = express();
 const path = require('path');
 
-var distDir = __dirname + "/dist/";
+var distDir = __dirname + "/dist/fnio-heroku-angular";
 app.use(express.static(distDir));
-app.listen(process.env.PORT || 8080);
+
 
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname + '/dist/fnio-heroku-angular/index.html'));
 });
+app.listen(process.env.PORT || 8080);
